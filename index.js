9c63434133266776033229d4e8a35617df3b4cc3
Laiba Shah <lshah1@fordham.edu>
2020-08-15 01:26:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+// ./js/index.js
+
+var name = "Susan"
+var height = "74"
+
+ if (name === "Susan") {
+   console.log("The name is correct")
+ }
+
+else {
+  console.log("Expected the name to be Susan")
+}
+
+// Don't worry about this
+module.exports = { name, height
+}
